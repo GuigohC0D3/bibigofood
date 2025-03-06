848f0418eadd25823f49342ed469b8ce587ee47b
Guilherme Ancheschi Werneck Pereira <guilhermewerneckpereira11@hotmail.com>
2025-03-06 20:50:35 -0300

    Coloquei uma animação no ban-vindo

--- app/welcome/index.tsx
+++ app/welcome/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { useRouter } from "expo-router";
+import LottieView from "lottie-react-native";
 
 export default function WelcomeScreen() {
   const router = useRouter();
@@ -10,13 +11,21 @@ export default function WelcomeScreen() {
 
     setTimeout(() => {
       console.log("Redirecionando para Cadastro...");
-      router.replace("/auth/Login"); // Agora vai para o Cadastro
+      router.replace("/auth/Login");
     }, 5000);
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Bem-vindo ao BibigoFood!</Text>
+      <Text style={styles.text}>Bem-vindo ao {"\n"}BibigoFood!</Text>
+      
+      {/* Animação de motoboy abaixo do texto */}
+      <LottieView
+        source={require("../../assets/animations/Animation - 1741304715761.json")} // Atualize o caminho se necessário
+        autoPlay
+        loop
+        style={styles.animation}
+      />
     </View>
   );
 }
@@ -32,5 +41,12 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     color: "#333",
+    textAlign: "center",
+    marginBottom: 20, // Espaço entre o texto e a animação
+  },
+  animation: {
+    width: 200, // Ajuste conforme necessário
+    height: 200,
   },
 });
+
